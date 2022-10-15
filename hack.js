289edbccc6743d73602ff09356bf8e062dc775aa
filime <36452369+filime@users.noreply.github.com>
2022-10-15 16:35:13 +0900

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,12 +1,1 @@
-var alasql = require('alasql');
-var fs = require('fs');
-
-var data = [{
-   "id": 1255,
-   "title": "The Brain and Nervous System (LS1.D)",
-   // ...
-},
-{
-   "id": 1256,
-    // ...
-}];
+alasql("CREATE TABLE asdf")

e47c54e86f43faaaf17e0c9605d6b13642390070
filime <36452369+filime@users.noreply.github.com>
2022-10-15 16:24:29 +0900

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,12 @@
+var alasql = require('alasql');
+var fs = require('fs');
+
+var data = [{
+   "id": 1255,
+   "title": "The Brain and Nervous System (LS1.D)",
+   // ...
+},
+{
+   "id": 1256,
+    // ...
+}];

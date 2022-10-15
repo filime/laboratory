d928e201addb41746a7c9949697d9726532a143e
filime <36452369+filime@users.noreply.github.com>
2022-10-15 16:22:47 +0900

    Update ha.js

--- ha.js
+++ ha.js
@@ -1,1 +1,11 @@
-import http from 'http';
+var alasql = require('alasql');
+
+var data = [{
+   "id": 1255,
+   "title": "The Brain and Nervous System (LS1.D)",
+   // ...
+},
+{
+   "id": 1256,
+    // ...
+}];

